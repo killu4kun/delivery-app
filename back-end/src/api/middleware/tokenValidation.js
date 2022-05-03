fbae31767582de2698785318b39f81c032dd4de4
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-05-03 15:52:40 -0300

    feat: implements a token validation

---
+++ back-end/src/api/middleware/tokenValidation.js
@@ -0,0 +1,16 @@
+const rescue = require('express-rescue');
+const jwt = require('jsonwebtoken');
+const { promises } = require('fs');
+const { errorResponse } = require('../utilities/generateResponse');
+
+module.exports = rescue(async (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) errorResponse(res, 'token', 'not found');
+  const jwtSecret = await promises.readFile('jwt.evaluation.key', 'utf8') || 'secret_key';
+
+  jwt.verify(token, jwtSecret, { algorithm: ['HS256'] }, (err, decoded) => {
+    if (err) errorResponse(res, 'token', 'expired or invalid');
+    res.locals.user = decoded.data.email;
+    next();
+  });
+});
--- back-end/src/api/utilities/generateResponse.js
+++ back-end/src/api/utilities/generateResponse.js
@@ -1,19 +1,21 @@
 const errors = {
   notFound: (data) => `${data} not found`,
   internal: (data) => data.message || 'Internal server error',
+  token: (data) => `Token ${data}`,
 };
 
 const codes = {
   ok: 200,
   create: 201,
   badRequest: 400,
+  token: 401,
   notFound: 404,
   internal: 500,
 };
 
 const errorResponse = (res, key, data) => {
   const code = codes[key];
-  const error = key === ('notFound' || 'internal') ? errors[key](data) : data;
+  const error = key === 'badRequest' ? data : errors[key](data);
   return res.status(code).json({ error });
 };
 
