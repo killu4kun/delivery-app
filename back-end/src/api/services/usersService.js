ce2235b90519c6aeefd5f273f1dc94aa52c09f85
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-05-04 04:41:34 -0300

    feat: add convert password to md5 hash

--- back-end/src/api/services/usersService.js
+++ back-end/src/api/services/usersService.js
@@ -1,10 +1,16 @@
+const md5 = require('md5');
 const { User } = require('../../database/models');
 
 const getUserByEmail = async (email) =>
   User.findOne({ where: { email }, attributes: { exclude: ['password'] } });
 
-const getUserByEmailAndPassword = async (email, password) =>
-  User.findOne({ where: { email, password }, attributes: { exclude: ['password'] } });
+const getUserByEmailAndPassword = async (email, password) => {
+  const hash = md5(password);
+  return User.findOne({
+    where: { email, password: hash },
+    attributes: { exclude: ['password'] },
+  });
+};
 
 module.exports = {
   getUserByEmail,
