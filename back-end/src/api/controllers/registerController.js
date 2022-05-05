794239f97097394920b3666f5e7dbef374e00cfa
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-05-05 14:44:10 -0300

    refactor: changes variable name to follow previous pattern

--- back-end/src/api/controllers/registerController.js
+++ back-end/src/api/controllers/registerController.js
@@ -4,7 +4,7 @@ const usersService = require('../services/usersService');
 module.exports = [
   rescue(async (req, res) => {
     const newUser = req.body;
-    const responseBody = await usersService.createUser(newUser);
-    res.status(201).json(responseBody);
+    const body = await usersService.createUser(newUser);
+    res.status(201).json(body);
   }),
 ];
