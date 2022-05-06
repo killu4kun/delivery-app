b5e2879b77703e35fb86a684f591f4f2dfb28ae5
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-05-06 14:35:17 -0300

    feat: add cors middleware

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,12 @@
 const app = require('express')();
 const bodyParser = require('body-parser').json();
+const cors = require('cors');
 const loginRouter = require('./routes/loginRouter');
 const salesRouter = require('./routes/salesRouter');
 const errorMiddleware = require('./middleware/errorMiddleware');
 
 app.use(bodyParser);
+app.use(cors());
 
 app.use('/login', loginRouter);
 app.use('/sales', salesRouter);
