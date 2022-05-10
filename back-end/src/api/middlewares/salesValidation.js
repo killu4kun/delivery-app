d09047ea48fad73b2e688fdb546c9b735dbafbb2
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-05-10 16:56:46 -0300

    refactor: add validateProductsById

--- back-end/src/api/middlewares/salesValidation.js
+++ back-end/src/api/middlewares/salesValidation.js
@@ -1,7 +1,9 @@
 const rescue = require('express-rescue');
 const { getUserById } = require('../services/usersService');
+const Product = require('../services/Products');
 const { userNotFound, sellerNotFound } = require('../errors/requestErrors');
 const salesSchema = require('../schemas/salesSchema');
+const { productNotFound } = require('../errors/productErros');
 
 const validateSale = rescue(async (req, _res, next) => {
   await salesSchema.validateAsync(req.body);
@@ -17,4 +19,15 @@ const validateUsersById = rescue(async (req, _res, next) => {
   next();
 });
 
-module.exports = [validateSale, validateUsersById];
+const validateProductsById = rescue(async (req, _res, next) => {
+  const { products } = req.body;
+  const db = await Product.getAll();
+  const dbIds = db.map((product) => product.id);
+  products.forEach((product) => {
+    const error = dbIds.includes(product.id);
+    if (!error) next(productNotFound);
+  });
+  next();
+});
+
+module.exports = [validateSale, validateUsersById, validateProductsById];
