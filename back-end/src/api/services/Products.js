2c9d7b37bd0e4d16cfe6f08da95b6fa7c563c224
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-05-10 16:31:53 -0300

    refactor: cchange throw location of err not found

--- back-end/src/api/services/Products.js
+++ back-end/src/api/services/Products.js
@@ -1,7 +1,12 @@
 const { Product } = require('../../database/models');
+const { productNotFound } = require('../errors/productErros');
 
 module.exports = {
   getAll: async () => Product.findAll(),
-  
-  getById: async (id) => Product.findOne({ where: { id } }),
+
+  getById: async (id) => {
+    const product = await Product.findByPk(id);
+    if (!product) throw productNotFound;
+    return product;
+  },
 };
