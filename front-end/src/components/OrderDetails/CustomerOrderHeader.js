adc2aecbda61cd380219bb8f7b94b5dcfa44f1a2
Dheniarley Cruz <dheniarley.ds@gamil.com>
2022-05-24 00:33:35 -0300

    Fix bud lint.

--- front-end/src/components/OrderDetails/CustomerOrderHeader.js
+++ front-end/src/components/OrderDetails/CustomerOrderHeader.js
@@ -2,48 +2,59 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../../styles/CustomerOrderHeader.css';
 
-const CustomerOrderHeader = ({ order }) => {
-  if(order.saleDate) {
-    console.log(order.saleDate.substr(0, 10));
-  }
-  return (
-    <div className="header-CustomOrder">
+const numMaxCharacters = 10;
+const CustomerOrderHeader = ({ order }) => (
+  <div className="header-CustomOrder">
+    <div
+      className="header-order"
+      data-testid="customer_order_details__element-order-details-label-order-id"
+    >
+      { `Pedido: ${order.id}` }
+    </div>
+    <div
+      className="header-saller"
+      data-testid="customer_order_details__element-order-details-label-seller-name"
+    >
+      { `P. Vend: ${order.sellerId}` }
+    </div>
+    <div className="header-date">
+      <span
+        className="text-date"
+        data-testid="customer_order_details__element-order-details-label-order-date"
+      >
+        { order.saleDate
+          && Intl.DateTimeFormat(
+            'pt-br',
+          ).format(new Date(order.saleDate.substr(0, numMaxCharacters))) }
+      </span>
+    </div>
+    <div className="header-status">
+      <span
+        className="header-status-text"
+        data-testid="customer_order_details__element-order-details-label-delivery-status"
+      >
+        { order.status }
+      </span>
+    </div>
+    <div className="header-change-status">
       <div
-        className="header-order"
-        data-testid="customer_order_details__element-order-details-label-order-id"
+        type="button"
+        className="button-change-status"
+        data-testid="customer_order_details__button-delivery-check"
       >
-        Pedido: { order.id }
-      </div>
-      <div 
-        className="header-saller"
-        data-testid="customer_order_details__element-order-details-label-seller-name"
-      >
-        P. Vend: { order.sellerId }
-      </div>
-      <div className="header-date">
-        <span className="text-date" data-testid="customer_order_details__element-order-details-label-order-date">
-         { order.saleDate && Intl.DateTimeFormat('pt-br').format(new Date(order.saleDate.substr(0,10))) }
-        </span>
-      </div>
-      <div className="header-status">
-        <span
-          className="header-status-text"
-          data-testid="customer_order_details__element-order-details-label-delivery-status"
-        >
-          { order.status }
-        </span>
-      </div>
-      <div className="header-change-status">
-        <div 
-          type="button"
-          className="button-change-status"
-          data-testid="customer_order_details__button-delivery-check"
-        >
-          MARCAR COMO ENTREGUE
-        </div>
+        MARCAR COMO ENTREGUE
       </div>
     </div>
-  );
+  </div>
+);
+
+CustomerOrderHeader.propTypes = {
+  order: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    sellerId: PropTypes.number.isRequired,
+    saleDate: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default CustomerOrderHeader;
